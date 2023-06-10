0c34079552f475002c22dec5007ccac9316e2664
Jason Smith <smija13@gmail.com>
2023-06-09 22:45:49 -0700

    repair file path for larger images in portfolio

--- js/portfolioData.js
+++ js/portfolioData.js
@@ -1,63 +1,63 @@
 let modalInfo = [
   {
     "id": 1,
-    "imgSrc": "./../images/lg/Hallway_LG.png",
+    "imgSrc": "./images/Resized/Hallway_LG.png",
     "imgAlt": "Evil Hallway",
     "year": 2022,
     "tools": ["Blender"]
   },
   {
     "id": 2,
-    "imgSrc": "./../images/lg/Reformation_Fire0.png",
+    "imgSrc": "./images/Resized/Reformation_Fire0.png",
     "imgAlt": "Fire Shot",
     "year": 2019,
     "tools": ["After Effects"]
   },
   {
     "id": 3,
-    "imgSrc": "./../images/lg/Reformation_Tentacle.png",
+    "imgSrc": "./images/Resized/Reformation_Tentacle.png",
     "imgAlt": "Tentacle Shot",
     "year": 2019,
     "tools": ["After Effects", "Blender", "Photoshop"]
   },
   {
     "id": 4,
-    "imgSrc": "./../images/lg/FacelessSelfPortrait_2013.png",
+    "imgSrc": "./images/Resized/FacelessSelfPortrait_2013.png",
     "imgAlt": "Faceless Self Portrait",
     "year": 2013,
     "tools": ["Oil"]
   },
   {
     "id": 5,
-    "imgSrc": "./../images/lg/Zombie_Block_Print_01_LG.png",
+    "imgSrc": "./images/Resized/Zombie_Block_Print_01_LG.png",
     "imgAlt": "Block Print",
     "year": 2020,
     "tools": ["Rubber Block", "Ink"]
   },
   {
     "id": 6,
-    "imgSrc": "./../images/lg/Cyanide_Robots_LG.png",
+    "imgSrc": "./images/Resized/Cyanide_Robots_LG.png",
     "imgAlt": "Cyanide Robots (Fake Band Album Cover)",
     "year": 2020,
     "tools": ["Acrylic", "Marker", "White Out"]
   },
   {
     "id": 7,
-    "imgSrc": "./../images/lg/Frankenstein_Monster_LG.png",
+    "imgSrc": "./images/Resized/Frankenstein_Monster_LG.png",
     "imgAlt": "Frankenstein's Monster",
     "year": 2013,
     "tools": ["Conte"]
   },
   {
     "id": 8,
-    "imgSrc": "./../images/lg/Pelvic_Oil_LG.png",
+    "imgSrc": "./images/Resized/Pelvic_Oil_LG.png",
     "imgAlt": "Pelvic Painting",
     "year": 2013,
     "tools": ["Oil"]
   },
   {
     "id": 9,
-    "imgSrc": "./../images/lg/My_Morning_Jacket_01_LG.png",
+    "imgSrc": "./images/Resized/My_Morning_Jacket_01_LG.png",
     "imgAlt": "My Morning Jacket (Fake Poster)",
     "year": 2013,
     "tools": ["Photoshop"]
